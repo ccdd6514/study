1760fc89e51ba6494764c7494d74b815f5ee1f53
ccddxiaoyu <ccddxiaoyu@muchenglin.com>
2023-02-28 08:33:07 +0800

    fix: form表单完成

--- vue-component/mycomponent/src/router/index.js
+++ vue-component/mycomponent/src/router/index.js
@@ -1,14 +1,12 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import HomeView from "../views/HomeView.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "home",
-    component: HomeView,
+    path: "/form",
+    component: () => import("../views/mForm.vue"),
   },
 ];
 
