af512bd178acb716307ab5dd58e2408dabc9ed68
ccddxiaoyu <ccddxiaoyu@muchenglin.com>
2023-07-10 21:35:08 +0800

    fix: 父子组件船只，瀑布流

--- vite-demo/src/App.vue
+++ vite-demo/src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <Layout></Layout>
+  <!-- <Layout></Layout> -->
+  <waterFall></waterFall>
 </template>
 
 <script setup lang='ts'>
 import { reactive } from "vue"
-import Layout from './Layout/index.vue'
+// import Layout from './Layout/index.vue'
+import waterFall from './components/waterFall/index.vue'
 
 const man = reactive({ name: 'ccdd1', test: 'sdfds' })
 console.log(man)
