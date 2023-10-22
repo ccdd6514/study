8fc2842b57d053a941a5f0b7cc9a0384674cfdfc
ccddxiaoyu <ccddxiaoyu@muchenglin.com>
2023-10-22 16:42:29 +0800

    fi

---
+++ webpack/小册/5-2_use_ssr/src/entry-client.js
@@ -0,0 +1,7 @@
+import { createSSRApp } from "vue";
+
+import App from "./App.vue";
+
+
+
+createSSRApp(App).mount("#app");
---
+++ webpack/小册/5Section_Vue/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h3>{{ message }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { message: "Hello World" };
+  },
+};
+</script>
+
+<style>
+body {
+  background: pink;
+}
+h3 {
+  margin: 40px 0 0;
+  color: #42b983;
+}
+</style>
